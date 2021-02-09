b804f1b1e7c73972c7d47c96f77bcb1508cde33a
Sonia RM Karungi <soniakxxx@gmail.com>
2021-02-09 13:16:26 +0300

    1.3 intro-to-JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 let pokemonList = [
     {
         "name" : "Bulbasaur",
-        "height" : 7,
+        "height" : 0.7,
         "types" : ['grass', 'poison']
     },
     {
         "name" : "Ivysaur",
-        "height" : 1,
+        "height" : 1.7,
         "types" : ['grass', 'poison']
     },
     {
         "name" : "Charizard",
-        "height" : 2,
+        "height" : 0.2,
         "types" : ['fire', 'flying']
     }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    pokemonList[i].height > 1 
+    ? 
+    document.write(pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + ') - Wow, that’s big!' + "<br><br>") 
+    : 
+    document.write(pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + ')' + "<br><br>");
+    console.log(pokemonList[i]);
+
+}
